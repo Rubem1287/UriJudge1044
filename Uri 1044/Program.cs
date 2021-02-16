e1257770f80a00348e4e7b76308ab49d1ff619bf
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-15 21:05:36 -0300

    Add files via upload

---
+++ Uri 1044/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Uri_1044
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            string[] vetor = Console.ReadLine().Split(' ');
+            a = int.Parse(vetor[0]);
+            b = int.Parse(vetor[1]);
+
+            if (a % 2 == 0 && b % 2 == 0)
+            {
+                Console.WriteLine("São Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Não São Multiplos");
+            }
+
+           
+        }
+    }
+}
